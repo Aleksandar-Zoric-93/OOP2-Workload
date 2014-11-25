0363c5b864f91fd49d5c4f7bed0d988b98c0a79e
unknown <t00166011@1XXB8CA3A8A4D65.students.ittralee.local>
2014-11-25 10:43:18 +0000

    'update'

---
+++ CreditCard.java
@@ -0,0 +1,102 @@
+//Aleksandar Zoric
+/* This program will create the CreditCard class for the project.  It will contain
+ * mutator, accessor and constructor methods, and also a toString() method to 
+ * display it in a specific format */
+ 
+  import java.io.*;
+  
+  public class CreditCard implements Serializable{
+ 	
+ 		//Attributes
+ 	private String forename;
+ 	private String surname;
+ 	private long cardNumber;
+ 	private int expiryDate;
+ 	private int cvv;
+ 	
+ 	
+ 		
+ 		//Constructors
+ 	public CreditCard()
+ 	{
+ 		this("Unknown","Unknown",0,0,0);	
+ 	}
+ 		
+ 		
+
+ 	public CreditCard(String forename, String surname, 
+ 				  long cardNumber, int expiryDate, int cvv)
+ 	{
+ 		setForename(forename);
+ 		setSurname(surname);
+ 		setCardNumber(cardNumber);
+ 		setExpiry(expiryDate);
+ 		setCVV(cvv);	
+ 	}
+ 	
+ 	
+ 	
+ 		//Mutator
+ 	public void setForename(String forename)
+ 	{
+ 		this.forename = forename;
+ 	}
+ 	
+ 	public void setSurname(String surname)
+ 	{
+ 		this.surname = surname;
+ 	}
+ 	
+ 	public void setCardNumber(long cardNumber)
+ 	{
+ 		this.cardNumber = cardNumber;
+ 	}
+ 	
+ 	public void setExpiry(int expiryDate)
+ 	{
+ 		this.expiryDate = expiryDate;
+ 	}
+ 	
+ 	public void setCVV(int cvv)
+ 	{
+ 		this.cvv = cvv;
+ 	}
+ 	
+ 	
+ 	
+ 		//Accessor
+ 	public String getForename()
+ 	{
+ 		return forename;
+ 	}
+ 	
+ 	public String getSurname()
+ 	{
+ 		return surname;
+ 	}
+ 	
+ 	public long getCardNumber()
+ 	{
+ 		return cardNumber;
+ 	}
+ 	
+ 	public int getExpiryDate()
+ 	{
+ 		return expiryDate;
+ 	}
+ 	
+ 	public int getCVV()
+ 	{
+ 		return cvv;
+ 	}
+ 	
+ 	
+ 	
+ 	
+ 		//toString
+ 	public String toString()
+ 	{
+ 		return String.format("Forename: %s\nSurname: %s\nCard Number: %d\nExpiry Date: %d\nCVV number: %d",
+ 							   	getForename(),getSurname(),getCardNumber(),getExpiryDate(),getCVV());
+ 	}
+  }
